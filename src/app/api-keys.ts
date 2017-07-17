7f25fcbc0a9a7811f4dca4a2fe0ffb31a3367365
Lina Shadrach <lina.shadrach@gmail.com>
2017-07-17 16:41:56 -0700

    add day1 code

---
+++ src/app/api-keys.ts
@@ -0,0 +1,10 @@
+export const marsRoverKey = 'EjmTb8y2se5fEvm23Z1dZnXEiJ0SowStynXAIxqE';
+
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyD3BunMzko81TMTOaig97T7q6YcZZiIJ_k",
+  authDomain: "mars-rover-bd251.firebaseapp.com",
+  databaseURL: "https://mars-rover-bd251.firebaseio.com",
+  projectId: "mars-rover-bd251",
+  storageBucket: "mars-rover-bd251.appspot.com",
+  messagingSenderId: "885672183325"
+};
